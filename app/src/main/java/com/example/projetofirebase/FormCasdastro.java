0abc441a5bdc448f4ef01919a9a4b58ac83c6b7a
Ana Paula <ana.oliveira.prt_a@msft.cesae.pt>
2022-10-21 12:18:51 +0100

    Projeto Firebase

---
+++ app/src/main/java/com/example/projetofirebase/FormCasdastro.java
@@ -0,0 +1,16 @@
+package com.example.projetofirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormCasdastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_casdastro);
+
+        getSupportActionBar().hide();
+    }
+}
--- app/src/main/java/com/example/projetofirebase/FormLogin.java
+++ app/src/main/java/com/example/projetofirebase/FormLogin.java
@@ -2,13 +2,34 @@ package com.example.projetofirebase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class FormLogin extends AppCompatActivity {
+
+    private TextView text_tela_cadastro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_login);
+
+        getSupportActionBar().hide();
+        InciarComponentes();
+
+        text_tela_cadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(FormLogin.this, FormCasdastro.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void InciarComponentes(){
+        text_tela_cadastro = findViewById(R.id.text_tela_cadastro);
     }
 }
