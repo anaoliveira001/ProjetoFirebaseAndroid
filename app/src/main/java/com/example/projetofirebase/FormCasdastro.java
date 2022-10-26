3f228d717a87fdb1a9039c21cceb1883bf49a589
Ana Paula <ana.oliveira.prt_a@msft.cesae.pt>
2022-10-26 14:13:10 +0100

    Projeto Firebase

--- app/src/main/java/com/example/projetofirebase/FormCasdastro.java
+++ app/src/main/java/com/example/projetofirebase/FormCasdastro.java
@@ -1,10 +1,44 @@
 package com.example.projetofirebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.auth.FirebaseAuthCredentialsProvider;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class FormCasdastro extends AppCompatActivity {
+
+    // Criação de objetos para o Edit Text e Button
+
+    private EditText edit_nome, edit_email, edit_senha;
+    private Button bt_cadastro;
+    //Array criado para mensagem e erro e sucesso, esse array será buscado para o indice
+    //para determina ação
+    String [] mensagens ={"Please, fill in all the fields", "Successful Registration, Welcome"};
+    String usuarioID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +46,103 @@ public class FormCasdastro extends AppCompatActivity {
         setContentView(R.layout.activity_form_casdastro);
 
         getSupportActionBar().hide();
+        IniciarComponentes();
+
+        //evento para sinalização da autenticão de caracteres
+        bt_cadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String nome = edit_nome.getText().toString();
+                String email = edit_email.getText().toString();
+                String senha = edit_senha.getText().toString();
+
+                // evento para quando o algum desses 3 campos estiverem vazios
+                if(nome.isEmpty() || email.isEmpty() || senha.isEmpty()){
+                    Snackbar snackbar = Snackbar.make(view, mensagens[0], Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.GREEN);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+                }else {
+                    CadastrarUsuario(view);
+                }
+            }
+        });
+    }
+
+    // método para cadastrar Usuário no Firebase, tratar as excessões para mensagens de erros:
+    //email inválido, número insuficiente de caracteres para a senha
+    private void CadastrarUsuario(View view){
+
+        String email = edit_email.getText().toString();
+        String senha = edit_senha.getText().toString();
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if(task.isSuccessful()){
+
+                    SalvarDadosUsuario();
+
+                    Snackbar snackbar = Snackbar.make(view, mensagens[1], Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.GREEN);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+                }else{
+                    String erro;
+                    try {
+                        throw task.getException();
+                    } catch (FirebaseAuthWeakPasswordException e){
+                        erro = "Enter a password with at least 6 characters";
+                    }catch (FirebaseAuthUserCollisionException e){
+                        erro = "This account is already registered";
+                    }catch (FirebaseAuthInvalidCredentialsException e){
+                        erro = "Invalid e-mail, try again please";
+                    }
+                    catch (Exception e){
+                        erro = "Sorry, user registration error, try again later";
+                    }
+                    Snackbar snackbar = Snackbar.make(view, erro, Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.RED);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+                }
+            }
+        });
+    }
+
+    // Salvar Dados no FireStore database
+    private void SalvarDadosUsuario(){
+        String nome = edit_nome.getText().toString();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        Map<String, Object> usuarios = new HashMap<>();
+
+        usuarios.put("nome", nome);
+        usuarioID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        DocumentReference documentReference = db.collection("Usuarios").document(usuarioID);
+        documentReference.set(usuarios).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                Log.d("db","Saving your data successfully");
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("db_error", "Sorry, your data was not saved" + e.toString());
+
+            }
+        });
+    }
+
+    // método Inciar componentes - Para organizção do código
+    private void IniciarComponentes(){
+        edit_nome = findViewById(R.id.edit_nome);
+        edit_email = findViewById(R.id.edit_email);
+        edit_senha = findViewById(R.id.edit_senha);
+        bt_cadastro = findViewById(R.id.bt_cadastrar);
     }
 }
--- app/src/main/java/com/example/projetofirebase/FormLogin.java
+++ app/src/main/java/com/example/projetofirebase/FormLogin.java
@@ -1,15 +1,33 @@
 package com.example.projetofirebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ButtonBarLayout;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class FormLogin extends AppCompatActivity {
 
     private TextView text_tela_cadastro;
+    private EditText edit_email, edit_senha;
+    private Button entrar;
+    private ProgressBar progressBar;
+    String [] mensagens ={"Please, fill in all the fields", "Successful Login, Welcome"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +45,66 @@ public class FormLogin extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        entrar.setOnClickListener(view -> {
+            String email = edit_email.getText().toString();
+            String senha = edit_senha.getText().toString();
+
+            if(email.isEmpty() || senha.isEmpty()){
+                Snackbar snackbar = Snackbar.make(view, mensagens[0], Snackbar.LENGTH_SHORT);
+                snackbar.setBackgroundTint(Color.GREEN);
+                snackbar.setTextColor(Color.BLACK);
+                snackbar.show();
+            }else{
+                AutenticarUsuario(view);
+            }
+        });
+    }
+
+    private void AutenticarUsuario(View view){
+        String email = edit_email.getText().toString();
+        String senha = edit_senha.getText().toString();
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    progressBar.setVisibility(View.VISIBLE);
+
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            TelaPrincipal();
+                        }
+                    },3000);
+                }else{
+                    String erro;
+
+                    try {
+                        throw task.getException();
+                    }catch (Exception e){
+                        erro = "Error, try again";
+                    }
+                    Snackbar snackbar = Snackbar.make(view, erro, Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.RED);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+                }
+            }
+        });
+    }
+
+    private void TelaPrincipal(){
+        Intent intent = new Intent(FormLogin.this,TelaPrincipal.class);
+        startActivity(intent);
+        finish();
     }
 
     private void InciarComponentes(){
         text_tela_cadastro = findViewById(R.id.text_tela_cadastro);
+        edit_email = findViewById(R.id.edit_email);
+        edit_senha = findViewById(R.id.edit_senha);
+        entrar = findViewById(R.id.entrar);
+        progressBar = findViewById(R.id.progressBar);
     }
 }
